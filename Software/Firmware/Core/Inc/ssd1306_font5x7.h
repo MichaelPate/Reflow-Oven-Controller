228d2d99c07c83d2a690a80e003deecde2cfb12a
Michael Pate <pate5800@gmail.com>
2026-01-25 12:51:25 -0700

    Add OLED source code, fonts, methods. OLED is working with simple Hello World text

---
+++ Software/Firmware/Core/Inc/ssd1306_font5x7.h
@@ -0,0 +1,11 @@
+#ifndef SSD1306_FONT5X7_H
+#define SSD1306_FONT5X7_H
+
+#include <stdint.h>
+
+#define FONT5X7_WIDTH   5
+#define FONT5X7_HEIGHT  7
+
+extern const uint8_t font5x7[][FONT5X7_WIDTH];
+
+#endif
